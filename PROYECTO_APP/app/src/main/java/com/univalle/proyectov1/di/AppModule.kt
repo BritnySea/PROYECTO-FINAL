c690d8b47da4f314550d412af91ae34140d564f0
BritnySea <britny2003sea@gmail.com>
2026-05-12 23:49:53 -0400

    Apuntar app Android a servidor Railway en producción

--- PROYECTO_APP/app/src/main/java/com/univalle/proyectov1/di/AppModule.kt
+++ PROYECTO_APP/app/src/main/java/com/univalle/proyectov1/di/AppModule.kt
@@ -61,7 +61,7 @@ object AppModule {
         // Para emulador: http://10.0.2.2:8000/
         // Para dispositivo real: IP local de la PC en la misma red WiFi (ver con ipconfig)
         return Retrofit.Builder()
-            .baseUrl("http://192.168.0.20:8000/")
+            .baseUrl("https://refugiowoof.up.railway.app/")
             .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
